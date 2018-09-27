03d531d1118d4bcba4127536994c62f46dd5aa02
ebrahimasifismail <ebrahimasifismail@gmail.com>
2018-09-27 18:50:30 +0530

    initial commit

---
+++ rango1/rangoapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from rangoapp.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
+
---
+++ rango1/rangoapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Category, Page
+
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
